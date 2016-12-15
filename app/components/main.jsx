5ee2c8a6481b9f379eba06c78468cffb98641cc1
Valeria Pérez <valeria@can-do.consulting>
2016-12-15 11:51:39 -0600

    avance con error no modificado

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Layout extends React.Component {
-  constructor() {
-    super();
-    this.name = "Valeria";
-  }
-  render () {
-    return (
-      <h1> Aprendiendo React {this.name} </h1>
-      //var div = document.createElement("h1")
-      //div innerHTML = "some content"
-    );
-  }
-}
+import Layout from '.app/components/Layout';
+
 
 ReactDOM.render(<Layout/>, document.getElementById('container'));
--- public/build.js
+++ public/build.js
@@ -46,8 +46,6 @@
 
 	'use strict';
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 	var _react = __webpack_require__(1);
 
 	var _react2 = _interopRequireDefault(_react);
@@ -56,46 +54,13 @@
 
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 
+	var _Layout = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \".app/components/Layout\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+	var _Layout2 = _interopRequireDefault(_Layout);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var Layout = function (_React$Component) {
-	  _inherits(Layout, _React$Component);
-
-	  function Layout() {
-	    _classCallCheck(this, Layout);
-
-	    var _this = _possibleConstructorReturn(this, (Layout.__proto__ || Object.getPrototypeOf(Layout)).call(this));
-
-	    _this.name = "Valeria";
-	    return _this;
-	  }
-
-	  _createClass(Layout, [{
-	    key: 'render',
-	    value: function render() {
-	      return _react2.default.createElement(
-	        'h1',
-	        null,
-	        ' Aprendiendo React ',
-	        this.name,
-	        ' '
-	      )
-	      //var div = document.createElement("h1")
-	      //div innerHTML = "some content"
-	      ;
-	    }
-	  }]);
-
-	  return Layout;
-	}(_react2.default.Component);
-
-	_reactDom2.default.render(_react2.default.createElement(Layout, null), document.getElementById('container'));
+	_reactDom2.default.render(_react2.default.createElement(_Layout2.default, null), document.getElementById('container'));
 
 /***/ },
 /* 1 */
